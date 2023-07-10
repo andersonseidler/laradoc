37e423f2dd6b4c28dda218824fd4dec3b872fa5f
Anderson <andersonqipoa@gmail.com>
2023-07-10 19:38:52 -0300

    Att: Views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,134 @@
+
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
+
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <title>@yield('title')</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+        <meta content="Coderthemes" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ url('images/favicon.png') }}">
+        <!-- Daterangepicker css -->
+        <link rel="stylesheet" href="{{ url('assets/vendor/daterangepicker/daterangepicker.css') }}">
+        <!-- Vector Map css -->
+        <link rel="stylesheet" href="{{ url('assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}">
+        <!-- Theme Config Js -->
+        <script src="{{ url('assets/js/hyper-config.js') }}"></script>
+        <!-- App css -->
+        <link href="{{ url('assets/css/app-saas.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
+        <!-- Icons css -->
+        <link href="{{ url('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- Script -->
+        <script src="https://kit.fontawesome.com/6c4df5f46b.js" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' type='text/javascript'></script>
+        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="{{ url('js/mask-cep.js') }}"></script>
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Select2 css -->
+        <link href="{{ url('assets/vendor/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!--  Select2 Js -->
+        <script src="{{ url('assets/vendor/select2/js/select2.min.js')}}"></script>
+        <link href="assets/vendor/fullcalendar/main.min.css" rel="stylesheet" type="text/css">
+        
+        <style>
+            
+            *,
+            *::before,
+            *::after {
+                box-sizing: border-box;
+            }
+        
+            .filter-select {
+                background-color: transparent;
+                border: none;
+                padding: 0 1em 0 0;
+                margin: 0;
+                font-family: inherit;
+                font-size: inherit;
+                cursor: inherit;
+                width: auto;
+                line-height: inherit;
+                outline: none;
+                color: var(--ct-body-color);
+            }
+        
+            .filter-btn {
+                padding: 0.25em 0.5em;
+                font-size: inherit;
+                cursor: pointer;
+                border-radius: 4px;
+                color: var(--ct-body-color);
+                border: none;
+                color: #333;
+            }
+        
+            .card {
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+                border: none;
+            }
+        
+            .card-body {
+                padding: 0.5rem;
+            }
+        
+            .filter-select-container {
+                display: flex;
+                align-items: center;
+                gap: 0.5rem;
+            }
+    
+        </style>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+    <body>
+        <!-- Begin page -->
+        <div class="wrapper">
+            <!-- ========== Topbar Start ========== -->
+            @include('components.navbar')
+            <!-- ========== Topbar End ========== -->
+
+            <!-- ========== Left Sidebar Start ========== -->
+            @include('components.sidebar')
+            <!-- ========== Left Sidebar End ========== -->
+
+            <div class="content-page">
+                <div class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box">
+                                    
+                                    @include('sweetalert::alert')
+                                    @yield('content')
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </header>
-            @endif
+                </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+                <!-- Footer Start -->
+                @include('components.footer')
+                <!-- end Footer -->
+
+            </div>
+        </div> 
+        <!-- Chart js --> 
+        <script src="{{ url('assets/vendor/chart.js/chart.min.js') }}"></script>
+        <!-- Vendor js -->
+        <script src="{{ url('assets/js/vendor.min.js') }}"></script>   
+        <!-- plugin js -->
+        <script src="{{ url('assets/vendor/dropzone/min/dropzone.min.js') }}"></script>
+        <!-- init js -->
+        <script src="{{ url('assets/js/ui/component.fileupload.js') }}"></script>
+        <!-- App js -->
+        <script src="{{ url('assets/js/app.min.js') }}"></script>
+        <script src="assets/vendor/fullcalendar/main.min.js"></script>
+        {{-- <script src="assets/js/pages/demo.calendar.js"></script> --}}
     </body>
-</html>
+</html> 
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.app')
+
+@section('title', 'Usuários')
+
+@section('content')
+
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box">
+            <div class="page-title-right">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Usuários</li>
+                </ol>
+            </div>
+            <h3 class="page-title">Usuários</h3>
+        </div>
+    </div>
+</div>
+<br>
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="mb-3">
+                    <form action="{{ route('users.index') }}" method="GET">
+                        <div class="input-group">
+                            <input type="text" name="search" placeholder="Pesquisar" class="form-control">
+                            <button type="submit" class="btn btn-primary">Pesquisar</button>    
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="mb-3 text-end">
+                    <a href="{{ route('users.create') }}" class="btn btn-success ">Cadastrar</a>
+                    <button class="btn btn-secondary" id="deleteAllSelectedRecord" disabled >Excluir</button> 
+                </div>
+            </div>
+        </div>
+        <br>
+        @include('users._partials.list-usuarios')
+    
+
+
+<script>
+    const deleteLinks = document.querySelectorAll('.delete-user');
+    
+    const handleDeleteUser = (event) => {
+      event.preventDefault();
+    
+      const deleteUrl = event.target.href;
+    
+      Swal.fire({
+        title: 'Excluír',
+        text: 'Tem certeza que deseja excluir esse usuário?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Sim',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(deleteUrl, {
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRF-TOKEN': '{{ csrf_token() }}'
+            }
+          })
+            .then(response => response.json())
+            .then(data => {
+              if (data.message) {
+                Swal.fire('Sucesso', data.message, 'success');
+    
+                const deletedRow = event.target.closest('tr');
+                deletedRow.remove();
+    
+                // Atualizar a lista de registros via AJAX
+                fetch('{{ route('users.index') }}')
+                  .then(response => response.text())
+                  .then(html => {
+                    const tableContainer = document.getElementById('table-container');
+                    tableContainer.innerHTML = html;
+                  })
+                  .catch(error => {
+                    console.error('Ocorreu um erro:', error);
+                  });
+              }
+            })
+            .catch(error => {
+              console.error('Ocorreu um erro:', error);
+            });
+        }
+      });
+    };
+    
+    deleteLinks.forEach(link => {
+      link.addEventListener('click', handleDeleteUser);
+    });
+    </script>
+@endsection
